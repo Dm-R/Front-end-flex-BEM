efa8a833604abae33d6177fd9a983f7384c7f25e
Dmitriy <rikovdmitri39@gmail.com>
2020-02-27 15:07:22 +0300

    Integrate Travis CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,79 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  
+
   module: {
     rules: [
       {
-        test: /\.js$/,        
+        test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.scss$/,
-          use: ['css-hot-loader',
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                hmr: process.env.NODE_ENV === 'development',
-                reloadAll: true
-              }
+        use: ['css-hot-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+              reloadAll: true,
             },
-            "css-loader",
-            "sass-loader"
-        ]
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpg|png)$/,
         use: [
-              {
-                loader: "file-loader",
-                options: {
-                  name: "img/[name].[ext]",
-                }
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[ext]',
             },
-            {
-              loader: "image-webpack-loader",
-              options: {
-                mozjpeg: {
-                  progressive: true,
-                  quality: 70
-                }
-              }
-            }
-        ]
-      }
-    ]
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "styles.css"
+      filename: 'styles.css',
     }),
   ],
   devServer: {
     before(app, server) {
-      server._watch(`src/index.html`);
+      // eslint-disable-next-line no-underscore-dangle
+      server._watch('src/index.html');
     },
     contentBase: './dist',
     port: 1111,
-    hot: true
-  }
+    hot: true,
+  },
 };
